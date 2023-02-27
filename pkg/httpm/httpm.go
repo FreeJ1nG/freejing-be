563cc9c955b57a25d959187dd49184ac01ef10d5
valodeals <valodealss@gmail.com>
2023-02-27 12:17:05 +0000

    refactor: add nil case

--- pkg/httpm/httpm.go
+++ pkg/httpm/httpm.go
@@ -23,23 +23,25 @@ func MakeErrorResponse(w http.ResponseWriter, httpStatus int, err error) []byte 
 
 func MakeSuccessResponse[T any](w http.ResponseWriter, httpStatus int, data interface{}) []byte {
 	var response Response
-	switch v := data.(type) {
-	case T:
+	if data == nil {
 		w.WriteHeader(httpStatus)
-		response = Response{Data: v, StatusCode: httpStatus, Success: true}
-	case []T:
-		w.WriteHeader(httpStatus)
-		if len(v) == 0 {
-			response = Response{Data: []T{}, StatusCode: httpStatus, Success: true}
-		} else {
+		response = Response{StatusCode: httpStatus, Success: true}
+	} else {
+		switch v := data.(type) {
+		case T:
+			w.WriteHeader(httpStatus)
 			response = Response{Data: v, StatusCode: httpStatus, Success: true}
+		case []T:
+			w.WriteHeader(httpStatus)
+			if len(v) == 0 {
+				response = Response{Data: []T{}, StatusCode: httpStatus, Success: true}
+			} else {
+				response = Response{Data: v, StatusCode: httpStatus, Success: true}
+			}
+		default:
+			w.WriteHeader(http.StatusInternalServerError)
+			response = Response{Data: v, StatusCode: http.StatusInternalServerError, Success: false, Error: "invalid data type"}
 		}
-	case nil:
-		w.WriteHeader(httpStatus)
-		response = Response{Data: nil, StatusCode: httpStatus, Success: true}
-	default:
-		w.WriteHeader(http.StatusInternalServerError)
-		response = Response{Data: v, StatusCode: http.StatusInternalServerError, Success: false, Error: "invalid data type"}
 	}
 
 	json, _ := json.Marshal(response)
