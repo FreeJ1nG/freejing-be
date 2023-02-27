fd5c8bf63464b5919265beed584f658643b225b8
FreeJ1nG <Andrewjeremy12345@gmail.com>
2023-02-27 19:40:36 +0700

    feat: add golang migrate to project

---
+++ db/migration/000001_init_migration.up.sql
@@ -0,0 +1,18 @@
+CREATE TABLE blogs (
+  id BIGSERIAL PRIMARY KEY,
+  title TEXT NOT NULL,
+  content TEXT NOT NULL,
+  create_date DATE NOT NULL
+);
+CREATE TABLE users (
+  id BIGSERIAL PRIMARY KEY,
+  username TEXT UNIQUE NOT NULL,
+  email TEXT NOT NULL,
+  password_hash TEXT NOT NULL
+);
+CREATE TABLE chat_history (
+  id BIGSERIAL PRIMARY KEY,
+  sender TEXT NOT NULL,
+  message TEXT NOT NULL,
+  create_date DATE NOT NULL
+);
